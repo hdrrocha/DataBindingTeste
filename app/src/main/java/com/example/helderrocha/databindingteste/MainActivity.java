373f4d6023621f4b76d1f9c4fff2c336507bdea0
Heder Rocha <hedie.strokes@gmail.com>
2018-07-21 17:00:33 -0300

    first test data bifing

--- app/src/main/java/com/example/helderrocha/databindingteste/MainActivity.java
+++ app/src/main/java/com/example/helderrocha/databindingteste/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.helderrocha.databindingteste;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.helderrocha.databindingteste.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.HelloWorld.setText("1 2 3, um passito pra frente maria");
     }
 }
